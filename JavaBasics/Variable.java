a9d38b0b194f7dade5085c6b0c8ab9926ca06620
Soumyajit Manna <122715874+soumyajit-manna@users.noreply.github.com>
2024-12-27 19:55:35 +0530

    Add files via upload

---
+++ JavaBasics/Variable.java
@@ -0,0 +1,10 @@
+public class Variable {
+    public static void main(String args[]){
+        int a = 10;
+        int b = 15;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "Soumyajit";
+        System.out.println(name);
+    }
+}
