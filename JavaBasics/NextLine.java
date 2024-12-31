4ee7f578598d94d8248f4cbbd7cf0deb5e645325
Soumyajit Manna <122715874+soumyajit-manna@users.noreply.github.com>
2024-12-31 20:01:26 +0530

    Delete JavaBasics directory

--- JavaBasics/NextLine.java
+++
@@ -1,7 +0,0 @@
-public class NextLine{
-    public static void main(String args[]){
-        System.out.println("Hello Rahul!");
-        System.out.print("Hello Rai!\n");
-        System.out.print("Hello Sagnik1");
-    }
-}
--- JavaBasics/PrintHelloWorld.java
+++
@@ -1,7 +0,0 @@
-// Print Hello World
-
-public class PrintHelloWorld {
-    public static void main (String args[]){
-        System.out.print("Hello World!");
-    }
-}
