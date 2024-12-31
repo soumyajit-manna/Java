cba3550d974541df22a298bbb902fd86ac5b0428
Soumyajit Manna <122715874+soumyajit-manna@users.noreply.github.com>
2024-12-31 20:03:11 +0530

    Add files via upload

---
+++ TypeCasting/Problem4.java
@@ -0,0 +1,9 @@
+
+
+public class Problem4{
+    public static void main (String args[]){
+        byte b = 5;
+        byte a = b*2; // byte a = (byte) (b * 2); // Explicit cast to byte
+        System.out.println(a);
+    }
+} 
